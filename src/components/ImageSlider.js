51835dc15f2a9e0580e813ac93bda3f18a0d897a
Muhammad Usman Aslam <musmanaslam520@gmail.com>
2021-09-05 22:34:10 +0500

    Just initialized the project and just created files for project for now

---
+++ src/components/ImageSlider.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ImageSlider() {
+	return (
+		<div>
+			<h1>Image Slider</h1>
+		</div>
+	);
+}
+
+export default ImageSlider;
---
+++ src/components/SliderData.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SliderData() {
+	return (
+		<div>
+			<h1>Slider Data</h1>
+		</div>
+	);
+}
+
+export default SliderData;
